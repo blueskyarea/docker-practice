87eebff0c22b92216eadb2f701f9de6577ef7a8b
mh <blueskyareahm@gmail.com>
2018-03-28 00:23:50 +0900

    NodeManager container

--- hadoop/2.7.3/cmd.sh
+++ hadoop/2.7.3/cmd.sh
@@ -2,13 +2,15 @@
 set -e
 
 if [ "$TAG" = 'RM' ]; then
-  exec /usr/hdp/2.6.2.0-205/hadoop-yarn/sbin/yarn-daemon.sh start resourcemanager &
-  rm -rf /tmp/hadoop-root/dfs/data/current &
-  exec /usr/hdp/2.6.2.0-205/hadoop/bin/hdfs --config /etc/hadoop/conf namenode -format -f &
-  exec /usr/hdp/2.6.2.0-205/hadoop/sbin/hadoop-daemon.sh start namenode &
-  while true; do sleep 1000; done
+  /usr/hdp/2.6.2.0-205/hadoop-yarn/sbin/yarn-daemon.sh start resourcemanager &&
+  rm -rf /tmp/hadoop-root/dfs/data/current &&
+  /usr/hdp/2.6.2.0-205/hadoop/bin/hdfs --config /etc/hadoop/conf namenode -format &&
+  /usr/hdp/2.6.2.0-205/hadoop/sbin/hadoop-daemon.sh start namenode &&
+  hdfs dfs -mkdir /user &&
+  while true; do sleep 1000; done 
 elif [ "$TAG" = 'NM' ]; then
-  exec /usr/hdp/2.6.2.0-205/hadoop-yarn/sbin/yarn-daemon.sh start nodemanager &
+  /usr/hdp/2.6.2.0-205/hadoop-yarn/sbin/yarn-daemon.sh start nodemanager &&
+  /usr/hdp/2.6.2.0-205/hadoop/sbin/hadoop-daemon.sh start datanode &&
   while true; do sleep 1000; done
 fi
 
