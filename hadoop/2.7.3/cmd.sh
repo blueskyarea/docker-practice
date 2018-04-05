2e7d28ffc3999adbe09d756a62238a0d27be22bb
mh <blueskyareahm@gmail.com>
2018-04-05 23:46:55 +0900

    node-manager is depends on resource-manager.

--- hadoop/2.7.3/cmd.sh
+++ hadoop/2.7.3/cmd.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$TAG" = 'RM' ]; then
   /usr/hdp/2.6.2.0-205/hadoop-yarn/sbin/yarn-daemon.sh start resourcemanager &&
   rm -rf /tmp/hadoop-root/dfs/data/current &&
-  /usr/hdp/2.6.2.0-205/hadoop/bin/hdfs --config /etc/hadoop/conf namenode -format &&
+  echo "Y" | /usr/hdp/2.6.2.0-205/hadoop/bin/hdfs --config /etc/hadoop/conf namenode -format &&
   /usr/hdp/2.6.2.0-205/hadoop/sbin/hadoop-daemon.sh start namenode &&
   #hdfs dfs -mkdir /user &&
   while true; do sleep 1000; done 
