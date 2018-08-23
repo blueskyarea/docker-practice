1d946fbd07feb8bb4bd9c0c0c9aa690c4a8e2d8d
mh <blueskyareahm@gmail.com>
2018-08-23 21:43:56 +0900

    hadoop adjustment.

--- hadoop/2.7.3/cmd.sh
+++ hadoop/2.7.3/cmd.sh
@@ -6,6 +6,7 @@ if [ "$TAG" = 'RM' ]; then
   rm -rf /tmp/hadoop-root/dfs/data/current &&
   echo "Y" | /usr/hdp/2.6.2.0-205/hadoop/bin/hdfs --config /etc/hadoop/conf namenode -format &&
   /usr/hdp/2.6.2.0-205/hadoop/sbin/hadoop-daemon.sh start namenode &&
+  sleep 60 &&
   /bin/bash /hdfs-setup.sh &&
   while true; do sleep 1000; done 
 elif [ "$TAG" = 'NM' ]; then
