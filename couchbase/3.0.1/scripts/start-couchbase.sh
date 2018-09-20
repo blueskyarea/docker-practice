cdc0f93265b9620a3864f84593961b2036880ae0
mh <blueskyareahm@gmail.com>
2018-09-21 01:54:45 +0900

    retry function for service wake up.

--- couchbase/3.0.1/scripts/start-couchbase.sh
+++ couchbase/3.0.1/scripts/start-couchbase.sh
@@ -5,21 +5,34 @@ ulimit -n 10240
 ulimit -c unlimited
 sudo /etc/init.d/couchbase-server start
 
+function retryRequest () {
+  count=0
+  retry_upper_limit=5
+  request=$1
+  while :
+  do
+    command="curl -L ${request} -o /dev/null -w %{http_code}\n -s"
+    res=$(${command})
+    if [ $res = 200 ] || [ $res = 202 ] ; then
+      break
+    fi
+    count=$(expr $count + 1)
+    if [ ${count} -gt ${retry_upper_limit} ] ; then
+      exit -1
+    fi
+    sleep 5s
+  done
+}
+
 # waiting for finishing to start couchbase-server
-sleep 30s
+retryRequest "http://localhost:8091"
 
 # Setup Administrator username and password
-curl -v -X POST http://localhost:8091/settings/web -d 'password=password&username=admin&port=8091'
-
-sleep 5s
+retryRequest "-X POST http://localhost:8091/settings/web -d password=password&username=admin&port=8091"
 
 # Setup Bucket
-curl -u admin:password -v -X POST http://localhost:8091/pools/default/buckets \
--d 'flushEnabled=1&threadsNumber=3&replicaIndex=0&replicaNumber=0&evictionPolicy=valueOnly&ramQuotaMB=597&bucketType=membase&name=default&authType=sasl&saslPassword='
-
-sleep 5s
+retryRequest "-u admin:password -v -X POST http://localhost:8091/pools/default/buckets -d flushEnabled=1&threadsNumber=3&replicaIndex=0&replicaNumber=0&evictionPolicy=valueOnly&ramQuotaMB=597&bucketType=membase&name=default&authType=sasl&saslPassword="
 
 # Setup Index RAM Quota
-curl -u admin:password -X POST http://localhost:8091/pools/default \
--d 'memoryQuota=5000' -d 'indexMemoryQuota=269'
+retryRequest "-u admin:password -v -X POST http://localhost:8091/pools/default -d memoryQuota=2000&indexMemoryQuota=269"
 
