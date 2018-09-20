832f58f5346887ba13f6e66fd89751307adce215
mh <blueskyareahm@gmail.com>
2018-09-21 00:17:59 +0900

    sleeping for couchbase wake up.

--- couchbase/3.0.1/scripts/start-couchbase.sh
+++ couchbase/3.0.1/scripts/start-couchbase.sh
@@ -5,19 +5,19 @@ ulimit -n 10240
 ulimit -c unlimited
 sudo /etc/init.d/couchbase-server start
 
-# TODO wait for finishing to start couchbase-server
+# waiting for finishing to start couchbase-server
 sleep 30s
 
 # Setup Administrator username and password
 curl -v -X POST http://localhost:8091/settings/web -d 'password=password&username=admin&port=8091'
 
-sleep 10s
+sleep 5s
 
 # Setup Bucket
 curl -u admin:password -v -X POST http://localhost:8091/pools/default/buckets \
 -d 'flushEnabled=1&threadsNumber=3&replicaIndex=0&replicaNumber=0&evictionPolicy=valueOnly&ramQuotaMB=597&bucketType=membase&name=default&authType=sasl&saslPassword='
 
-sleep 10s
+sleep 5s
 
 # Setup Index RAM Quota
 curl -u admin:password -X POST http://localhost:8091/pools/default \
