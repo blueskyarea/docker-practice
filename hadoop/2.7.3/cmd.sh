928d804a5eed4c6f8936e76e290106293b6c7c7f
mh <blueskyareahm@gmail.com>
2019-01-01 23:29:15 +0900

    Dockerfile.hbase

--- hadoop/2.7.3/cmd.sh
+++ hadoop/2.7.3/cmd.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
 
+function waitClusterStart() {
+  while :
+  do
+    res=$(curl -L 'http://container-hdp-rm1001:8088/ws/v1/cluster/metrics' -s)
+    check=`echo "$res" | sed 's/,/\n/g' | sed 's/}//g' | grep activeNodes | awk -F':' '{print $2}'`
+    if [ $check -gt 0 ] ; then
+      break
+    fi
+    echo "waiting to start cluster."
+    sleep 10s
+  done
+}
+
 if [ "$TAG" = 'RM' ]; then
   /usr/hdp/2.6.2.0-205/hadoop-yarn/sbin/yarn-daemon.sh start resourcemanager &&
   rm -rf /tmp/hadoop-root/dfs/data/current &&
@@ -17,10 +30,12 @@ elif [ "$TAG" = 'NM' ]; then
   /bin/bash /start-zookeeper.sh &&
   while true; do sleep 1000; done
 elif [ "$TAG" = 'HMASTER' ]; then
+  waitClusterStart &&
   /bin/bash /start-zookeeper.sh &&
   /bin/bash /start-hbase.sh &&
   while true; do sleep 1000; done
 elif [ "$TAG" = 'HUE' ]; then
+  waitClusterStart &&
   /bin/bash /etc/init.d/hue start &&
   while true; do sleep 1000; done
 else
