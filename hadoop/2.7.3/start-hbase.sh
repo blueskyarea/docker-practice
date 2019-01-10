a4cf548c526d6b4dc11cdff9fef945a278fd22e3
mh <blueskyareahm@gmail.com>
2019-01-11 00:01:03 +0900

    Dockerfile.kerberos

--- hadoop/2.7.3/start-hbase.sh
+++ hadoop/2.7.3/start-hbase.sh
@@ -5,3 +5,9 @@
 
 # start Region server
 /usr/hdp/current/hbase-regionserver/bin/hbase-daemon.sh start regionserver
+
+# start Thrift server
+/usr/hdp/current/hbase-master/bin/hbase-daemon.sh start thrift -p 9090 --infoport 9095
+
+# start REST server
+/usr/hdp/current/hbase-master/bin/hbase-daemon.sh start rest -p 8080 --infoport 8085
