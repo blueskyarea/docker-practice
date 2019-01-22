08df0ec9cf5d8d80471df2926d759bfa7ef60283
mh <blueskyareahm@gmail.com>
2019-01-23 01:18:24 +0900

    consider to use hbase.

--- hadoop/2.7.3/scripts/start-hbase.sh
+++ hadoop/2.7.3/scripts/start-hbase.sh
@@ -7,7 +7,11 @@
 /usr/hdp/current/hbase-regionserver/bin/hbase-daemon.sh start regionserver
 
 # start Thrift server
-/usr/hdp/current/hbase-master/bin/hbase-daemon.sh start thrift -p 9090 --infoport 9095
+#/usr/hdp/current/hbase-master/bin/hbase-daemon.sh start thrift -p 9090 --infoport 9095
 
 # start REST server
-/usr/hdp/current/hbase-master/bin/hbase-daemon.sh start rest -p 8080 --infoport 8085
+#/usr/hdp/current/hbase-master/bin/hbase-daemon.sh start rest -p 8080 --infoport 8085
+
+# initialize
+/usr/hdp/2.6.2.0-205/hbase/bin/hbase shell /init-hbase.txt
+
